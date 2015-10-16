cf977d0ef377e905e90a69aba8e9b278d95f8a0b
Erhan Justice <bountyx@gmail.com>
2015-10-16 03:53:04 -0700

    Code formatting only

--- tasks/lib/bower_assets.js
+++ tasks/lib/bower_assets.js
@@ -4,68 +4,73 @@ var path = require('path');
 var grunt = require('grunt');
 var packageMatcher = require('./package_matcher');
 
-var Assets = function(cwd, componentsDir) {
-  this._assets = {};
-  this._cwd = cwd;
-  this._componentsDir = componentsDir;
+var Assets = function (cwd, componentsDir) {
+    this._assets = {};
+    this._cwd = cwd;
+    this._componentsDir = componentsDir;
 };
 
-Assets.prototype.addOverridden = function(override, pkg) {
-  var pkgPath = path.join(this._componentsDir, pkg);
+Assets.prototype.addOverridden = function (override, pkg) {
+    var pkgPath = path.join(this._componentsDir, pkg);
 
-  _(override).each(function(overriddenPaths, assetType) {
-    this.addAssets(overriddenPaths, pkg, assetType, pkgPath);
-  }, this);
+    _(override).each(function (overriddenPaths, assetType) {
+        this.addAssets(overriddenPaths, pkg, assetType, pkgPath);
+    }, this);
 };
 
-Assets.prototype.addUntyped = function(pkgFiles, pkg) {
-  this.addAssets(pkgFiles, pkg, '__untyped__');
+Assets.prototype.addUntyped = function (pkgFiles, pkg) {
+    this.addAssets(pkgFiles, pkg, '__untyped__');
 };
 
-Assets.prototype.addAssets = function(filePatterns, pkg, assetType, pkgPath) {
-  pkgPath = pkgPath || '';
+Assets.prototype.addAssets = function (filePatterns, pkg, assetType, pkgPath) {
+    pkgPath = pkgPath || '';
 
-  if (!_.isArray(filePatterns)) {
-    filePatterns = [ filePatterns ];
-  }
+    if (!_.isArray(filePatterns)) {
+        filePatterns = [filePatterns];
+    }
 
-  var basePath = path.join(this._cwd, pkgPath);
+    var basePath = path.join(this._cwd, pkgPath);
 
-  this._assets[assetType] = this._assets[assetType] || {};
-  this._assets[assetType][pkg] = _(grunt.file.expand({cwd: basePath}, filePatterns)).map(function(expandedPath) {
-    return path.join(pkgPath, expandedPath);
-  });
+    this._assets[assetType] = this._assets[assetType] || {};
+    this._assets[assetType][pkg] = _(grunt.file.expand({
+        cwd: basePath
+    }, filePatterns)).map(function (expandedPath) {
+        return path.join(pkgPath, expandedPath);
+    });
 };
 
-Assets.prototype.toObject = function() {
-  return _.clone(this._assets);
+Assets.prototype.toObject = function () {
+    return _.clone(this._assets);
 };
 
 
-var BowerAssets = function(bower, cwd) {
-  this.bower = bower;
-  this.cwd = cwd;
-  this.config = bower.config.json || 'bower.json';
-  this.assets = new Assets(cwd, bower.config.directory);
+var BowerAssets = function (bower, cwd) {
+    this.bower = bower;
+    this.cwd = cwd;
+    this.config = bower.config.json || 'bower.json';
+    this.assets = new Assets(cwd, bower.config.directory);
 };
 
 BowerAssets.prototype = Object.create(Emitter.prototype);
 BowerAssets.prototype.constructor = BowerAssets;
 
-BowerAssets.prototype.get = function() {
-  var bower = this.bower;
-  var bowerConfig = grunt.file.readJSON(path.join(this.cwd, this.config));
-  var exportsOverride = bowerConfig.exportsOverride;
+BowerAssets.prototype.get = function () {
+    var bower = this.bower;
+    var bowerConfig = grunt.file.readJSON(path.join(this.cwd, this.config));
+    var exportsOverride = bowerConfig.exportsOverride;
 
-  var paths = bower.commands.list({paths: true});
-  paths.on('end', function(data) {
-    this.emit('end', this.mergePaths(data, exportsOverride ? exportsOverride : {}));
-  }.bind(this));
-  paths.on('error', function(err) {
-    this.emit('error', err);
-  }.bind(this));
 
-  return this;
+    var paths = bower.commands.list({
+        paths: true
+    });
+    paths.on('end', function (data) {
+        this.emit('end', this.mergePaths(data, exportsOverride ? exportsOverride : {}));
+    }.bind(this));
+    paths.on('error', function (err) {
+        this.emit('error', err);
+    }.bind(this));
+
+    return this;
 };
 
 /**
@@ -75,24 +80,24 @@ BowerAssets.prototype.get = function() {
  *
  * @returns assets grouped by component and type
  */
-BowerAssets.prototype.mergePaths = function(bowerComponents, overrides) {
-  var findOverride = function(pkg) {
-    return _(overrides).find(function(override, override_key) {
-      return packageMatcher.matches(pkg, override_key);
-    });
-  };
+BowerAssets.prototype.mergePaths = function (bowerComponents, overrides) {
+    var findOverride = function (pkg) {
+        return _(overrides).find(function (override, override_key) {
+            return packageMatcher.matches(pkg, override_key);
+        });
+    };
 
-  _(bowerComponents).each(function(pkgFiles, pkg) {
-    var activeOverride = findOverride(pkg);
+    _(bowerComponents).each(function (pkgFiles, pkg) {
+        var activeOverride = findOverride(pkg);
 
-    if (activeOverride) {
-      this.assets.addOverridden(activeOverride, pkg);
-    } else {
-      this.assets.addUntyped(pkgFiles, pkg);
-    }
-  }, this);
+        if (activeOverride) {
+            this.assets.addOverridden(activeOverride, pkg);
+        } else {
+            this.assets.addUntyped(pkgFiles, pkg);
+        }
+    }, this);
 
-  return this.assets.toObject();
+    return this.assets.toObject();
 };
 
 module.exports = BowerAssets;
